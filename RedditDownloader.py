88f4437e6a4bbb2ab01e4088ffb03ef7c26519c1
gsingh9198 <gurneeshchahal09@gmail.com>
2018-04-20 20:44:26 +0530

    Update RedditDownloader.py

--- RedditDownloader.py
+++ RedditDownloader.py
@@ -58,11 +58,10 @@ class RedditDownloader:
 if __name__ == '__main__': #url = 'https://www.reddit.com/r/IndianTeens.json'
     args = sys.argv
     downloader = RedditDownloader(args[1])
-    # https: //www.reddit.com/r/DesiBoners+IndianBabes+IndianFetish+IndianPorn+IndianTeens+IndiansGoneWild+NRIBabes+indiangirls+sareeNSFW.json
     #print(p[len(p) - 1].get('id'))
     FINISHED = False
     items = []
-    foldername = '/home/gurneesh/Desktop/' + args[1]
+    foldername = '../Downloads' + args[1]
     if not os.path.exists(foldername):
         os.mkdir(foldername)
     name = None
