09b6763da9663d2ee5b9194aee743ea99d839a41
gurneesh <gurneeshchahal09@gmail.com>
2018-07-07 20:19:16 +0530

    some changes

--- RedditDownloader.py
+++ RedditDownloader.py
@@ -1,10 +1,8 @@
+#!/usr/bin/python3
+
 from urllib.request import urlopen, urlretrieve, Request
 from json import JSONDecoder
-import datetime
 import os
-import subprocess
-import time
-import datetime
 import youtube_dl
 import io
 import re
@@ -15,16 +13,21 @@ class RedditDownloader:
         self.subr = subr
         self.url = 'https://www.reddit.com/r/' + self.subr + '.json'
 
-    def req(self, previd = ''): 
+    def req(self, previd = ''):
         reddit_url = '%s?after=t3_%s' % (self.url, previd)
         print(reddit_url)
-        rq = Request(reddit_url)
-        rq.add_header('User-agent', 'Stylesheet images downloader Py3 v1')
-        source = urlopen(rq).read()
+        try:
+            rq = Request(reddit_url)
+            rq.add_header('User-agent', 'Stylesheet images downloader Py3 v1')
+            source = urlopen(rq).read()
+        except Exception as e:
+            print(e)
+            return e
         json = source.decode('utf-8')
         data = JSONDecoder().decode(json)
         return data
 
+    #method to extract urls of all the images of photos from imgur ablums
     def extract_imgur_album_urls(self, album_url):
         match = re.match("(https?)\:\/\/(www\.)?(?:m\.)?imgur\.com/(a|gallery)/([a-zA-Z0-9]+)(#[0-9]+)?", album_url)
         key = match.group(4)
@@ -37,14 +40,21 @@ class RedditDownloader:
             image_urls.append('https://imgur.com/' + id[0] + id[1])
         return (image_urls)
 
+    #method to download video content using youtube-dl module
     def ydownload(self, link, foldername): #foldername = '/home/gurneesh/Downloads/' + url[25: -5]
         print(foldername)
-        ydl_opts = {'outtmpl': foldername+'/%(title)s.%(ext)s',}
+        ydl_opts = None
+        if 'gfycat' in link:
+            ydl_opts = {'outtmpl': foldername+'/%(webpage_url_basename)s.%(ext)s',}
+        else:
+            ydl_opts = {'outtmpl': foldername+'/%(title)s.%(ext)s',}
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([link])
 
+    #method to download any mime type whose url is directly available in the json of the page
     def download(self, url, name):
         if os.path.exists(name):
+            print('Already Downloaded ')
             return
         print('Attempting to download {}'.format(url))
         filedata = urlopen(url).read()
@@ -53,28 +63,31 @@ class RedditDownloader:
         filehandle.close()
         print('Successfully downloaded {}'.format(name))
 
-            
 
-if __name__ == '__main__':
-    args = sys.argv
-    downloader = RedditDownloader(args[1])
-    #print(p[len(p) - 1].get('id'))
+#main function to use all the methods of the class to scrape a subreddit.
+#TO DO reduce the code into methods of the above class.
+def main(subr):
+    downloader = RedditDownloader(subr)
     FINISHED = False
     items = []
-    foldername = '../Downloads/' + args[1]
+    #if os.path.exists(os.getcwd()+'/'+subr):
+    #    print('downloaded')
+    #    return
+    foldername = os.getcwd()+'/'+subr
     if not os.path.exists(foldername):
         os.mkdir(foldername)
     name = None
     temp = None
     previd = None
-    count = 0
     while not FINISHED:
         name = None
         if (len(items) == 0):
             data = downloader.req()
-            items = [x['data'] for x in data['data']['children']]
-            # print(len(items))
-            previd = items[len(items) - 1].get('id')
+            if(type(data)==dict):
+                items = [x['data'] for x in data['data']['children']]
+                previd = items[len(items) - 1].get('id')
+            else:
+                return
         else :
             temp = items[len(items) - 1].get('id')
             data = downloader.req(previd)
@@ -82,11 +95,10 @@ if __name__ == '__main__':
         if (len(items) > 1):
             previd = items[len(items) - 1].get('id')
         for item in items:
-            try: #time.sleep(4)
+            try:
                 print(item.get('url'))
                 if item.get('url'):
                     link = item.get('url')
-                    count+=1
                     id = item.get('id')
                     if link.endswith('.jpg'):
                         name = foldername + '/' + id + '.jpg'
@@ -116,4 +128,9 @@ if __name__ == '__main__':
             except Exception as e:
                 print(e)
         if (temp == previd):
+            print('Completly scrapped {}'.format(subr))
             FINISHED = True
+
+if __name__=='__main__':
+    args = sys.argv
+    main(args[1])
